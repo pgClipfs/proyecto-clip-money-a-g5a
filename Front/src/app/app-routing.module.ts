d750e5d1a15653ec5247531b0ca1460c21d45c04
PabloAimar <pablo.aimar@kdsoft.io>
2021-01-05 15:48:38 -0300

    merge v1

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
-import { DepositComponent } from './components/deposit/deposit.component';
-import { DepositocuentaComponent } from './components/deposit/depositocuenta/depositocuenta.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { AuthGuard } from './security/auth.guard';
-import {DepositRapiPagoComponent } from './components/deposit-rapi-pago/deposit-rapi-pago.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
   { path: 'registration', component: RegistrationComponent},
-  { path: 'deposit', component: DepositComponent, pathMatch: 'full'},
-  { path: 'depositocuenta', component: DepositocuentaComponent, pathMatch: 'full'},
-  { path: 'auth', component: AuthComponent},
-  { path: 'depositrp', component: DepositRapiPagoComponent}
+  { path: 'auth', component: AuthComponent}
 ];
 
 @NgModule({
