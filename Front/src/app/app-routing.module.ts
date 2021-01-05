c1ede7d201c802e003f377f5e3b656e5f858ec8c
PabloAimar <pablo.aimar@kdsoft.io>
2021-01-05 16:20:43 -0300

    merge v2

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { HomeComponent } from './components/home/home.component';
+import { DepositComponent } from './components/deposit/deposit.component';
+import { DepositocuentaComponent } from './components/deposit/depositocuenta/depositocuenta.component';
+import { DepositRapiPagoComponent } from './components/deposit-rapi-pago/deposit-rapi-pago.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { AuthGuard } from './security/auth.guard';
@@ -10,6 +13,9 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
   { path: 'registration', component: RegistrationComponent},
+  { path: 'deposit', component: DepositComponent},
+  { path: 'depositocuenta', component: DepositocuentaComponent},
+  { path: 'depositrp', component: DepositRapiPagoComponent},
   { path: 'auth', component: AuthComponent}
 ];
 
